7394bd74200717581ba1a3195993077798874c5f
Nathan Kessler <nathant93@gmail.com>
2021-04-22 18:48:23 -0400

    add macros.asm

---
+++ input-test/src/macros.asm
@@ -0,0 +1,30 @@
+; Wait for VRAM to be safe to write to
+; Busy wait - should change to use interrupt
+waitVRAM: MACRO
+    ld  a,[rSTAT]
+    and STATF_BUSY
+    jr  nz,@-4
+ENDM
+
+; src -> dest
+m_strcpy: MACRO
+    ld de, \1
+    ld hl, \2
+    call strcpy
+ENDM
+
+; Display a string if an input mask matches the most recent
+; input read at $C000.
+; 1 - Input mask (or then nz compare)
+; 2 - String to display if or result is nz
+; 3 - Destination address to display the string
+m_displayInput: MACRO
+    ld a, [$C000]
+    and a, \1
+    jr z, .pressed\@
+    m_strcpy ClearStr, \3
+    jr .done\@
+.pressed\@:
+    m_strcpy \2, \3
+.done\@
+ENDM
